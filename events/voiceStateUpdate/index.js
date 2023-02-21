ed0d378126d76edf59c5b90ce303fbb2bb679a9b
tete0611 <tete0600@naver.com>
2023-02-21 13:34:29 +0900

    임베드빌더 제작중, 랜덤매칭 채널변경

--- events/voiceStateUpdate/index.js
+++ events/voiceStateUpdate/index.js
@@ -8,8 +8,10 @@ module.exports = {
    * @param {import("discord.js").VoiceState} oldState
    * @param {import("discord.js").VoiceState} newState
    */
-  async execute(oldState, newState) {
-    if (oldState.channel?.parent?.name === '랜덤채널') {
+  async execute(oldState, _) {
+    const parentId = oldState.channel?.parent?.id;
+    const waitingRoomId = process.env.WAITING_ROOM_ID;
+    if (oldState.channel?.parent?.id === parentId && oldState.channel?.id !== waitingRoomId) {
       /** 이탈한 채널의 멤버가 혼자이면 채널 삭제*/
       if (oldState.channel?.members.map(v => v).length <= 1)
         try {
