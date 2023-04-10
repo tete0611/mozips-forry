c35d31c0c229524de461a58afc30ce57d7e96611
tete0611 <tete0600@naver.com>
2023-04-10 13:53:11 +0900

    불필요 코드 제거

--- events/messageCreate/clear.js
+++ events/messageCreate/clear.js
@@ -9,14 +9,14 @@ module.exports = {
    * @param {import('discord.js').Message} message
    */
   async execute(message) {
-    if (message.content !== '!채팅삭제') return;
-    if (!checkRole(message.member, 'Manager')) return;
-    const { channel } = message;
-    const messages = await channel.messages.fetch();
-    try {
-      await channel.bulkDelete(messages);
-    } catch (error) {
-      console.log(error);
-    }
+    // if (message.content !== '!채팅삭제') return;
+    // if (!checkRole(message.member, 'Manager')) return;
+    // const { channel } = message;
+    // const messages = await channel.messages.fetch();
+    // try {
+    //   await channel.bulkDelete(messages);
+    // } catch (error) {
+    //   console.log(error);
+    // }
   },
 };
