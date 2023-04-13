30cbcc6eda127f3b0f5383b2b30d9604325c81be
tete0611 <tete0600@naver.com>
2023-04-13 18:02:31 +0900

    스티키 문구 오타 수정

--- events/messageCreate/welcome.js
+++ events/messageCreate/welcome.js
@@ -18,7 +18,7 @@ module.exports = {
       const messagesToDelete = botMessages.filter(msg => msg.id !== message.id).map(msg => msg.id);
       if (messagesToDelete.length > 0) channel.bulkDelete(messagesToDelete);
       await channel.send({
-        content: `Hello! New villiger!👋🏻\nWelcome to mozips village!🏡\nMozips Village is a place where you can learn and practice Korean together.\nShall we greet our Mozips Villigers?💬\n1️⃣ Please let us know your name (nickname).\n2️⃣ How long have you been learning Korean?\n3️⃣ Which country are you from?\n4️⃣ Say a word to our villigers`,
+        content: `Hello! New villiager!👋🏻\nWelcome to mozips village!🏡\nMozips Village is a place where you can learn and practice Korean together.\nShall we greet our Mozips Villiagers?💬\n1️⃣ Please let us know your name (nickname).\n2️⃣ How long have you been learning Korean?\n3️⃣ Which country are you from?\n4️⃣ Say a word to our villiagers`,
       });
     }
   },
